2296551b14428bf196bb2a5cda2045362ec889d1
Gauri <dahakegauri4@gmail.com>
2024-12-17 22:48:19 +0530

    hellow

---
+++ Dec16/vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const firstName = "John";
+  return(
+  <>
+
+    <h1 className="heading">hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad esse nobis quibusdam, incidunt, explicabo odio aut consequuntur assumenda molestiae voluptates consequatur officiis, voluptatum velit eligendi sed fuga dolorem suscipit adipisci?
+
+    </p>
+    <h2>Hellow {firstName}</h2>
+    <h2>{6*6+7876876}</h2>
+
+  </>
+  )
+ 
+}
+
+export default App
