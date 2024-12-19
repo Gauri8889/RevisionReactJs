6a3b162986e6e6b52847243b5353ef1684b2bc78
Gauri <dahakegauri4@gmail.com>
2024-12-19 18:30:41 +0530

    ReactApp

---
+++ reactApp/src/Header.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import "./App.css";
+function Header(){
+    return(
+        <>
+        <Navbar/>
+        <h1>Hellow from Header Component</h1>
+        </>
+    )
+}
+export default Header;
---
+++ reactApp/src/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () =>{
+    return(
+        <>
+        <nav>
+            <h1>SkyScrapper</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Services</a></li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+export default Navbar;
